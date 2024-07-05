6e4ff38fe17113e4d86129d5df6213b8114c2906
Petar Stevovski <stevovski.petar@gmail.com>
2024-07-05 16:50:44 +0200

    Updated schema for 'user' table to add 'password' column

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -7,6 +7,7 @@ export const schemaUser = sqliteTable('user', {
 	first_name: text('first_name').notNull(),
 	last_name: text('last_name').notNull(),
 	email: text('email').notNull().unique(),
+	password: text("password").notNull(),
 	created_at: integer('created_at', { mode: 'timestamp' })
 		.notNull()
 		.default(sql`(unixepoch())`)
