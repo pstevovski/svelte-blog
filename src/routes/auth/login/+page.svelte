546b68b9a56e95961d56a853a44c97753c116312
Petar Stevovski <stevovski.petar@gmail.com>
2024-07-08 23:33:46 +0200

    Updated structure of auth login and register pages

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,31 +1,67 @@
+<script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+	import FormInput from '$lib/components/Forms/FormInput.svelte';
+	import Loader from '$lib/components/Loaders/Loader.svelte';
+
+	export let form;
+	let submitting: boolean = false;
+</script>
+
 <div class="p-6 mx-auto my-10 border border-slate-200 rounded bg-white max-w-[500px] w-full">
 	<h3 class="text-left font-semibold text-slate-600 text-xl">Login</h3>
-	<p class="text-left text-slate-400 text-sm mb-8">
-		Use your existing account to login.
-	</p>
+	<p class="text-left text-slate-400 text-sm mb-8">Use your existing account to login.</p>
 
-	<form action="/auth?/login" class="flex flex-col">
-    <label class="text-xs text-slate-700 mb-1"for="email">Email</label>
-		<input
-			type="text"
+	<form
+		action="/auth?/login"
+		method="POST"
+		class="flex flex-col"
+		use:enhance={() => {
+			submitting = true;
+
+			return async ({ update, result }) => {
+				await update();
+				submitting = false;
+
+				// Updates the form properties for this page
+				// with the results from the form submission in the
+				// defined named form action
+				await applyAction(result);
+			};
+		}}
+	>
+		<FormInput
+			name="email"
 			placeholder="john@doe.com"
-			required
-			class="p-2 border border-slate-200 rounded focus:outline-none mb-6"
+			label="Email"
+			type="email"
+			error={form?.errors?.email}
 		/>
 
-    <label class="text-xs text-slate-700 mb-1"for="password">Password</label>
-		<input
-			type="password"
-			placeholder=""
-			required
-			class="p-2 border border-slate-200 rounded focus:outline-none mb-6"
-		/>
-		
-    <div class="flex items-center justify-between">
-      <a href="/auth/forgot-password" class="inline-block mb-10 text-xs text-slate-400 hover:text-slate-700 duration-200">Forgot Password?</a>
-      <a href="/auth/register" class="inline-block mb-10 text-xs text-slate-400 hover:text-slate-700 duration-200">Sign Up</a>
-    </div>
+		<FormInput name="password" label="Password" error={form?.errors?.password} />
 
-    <button class="w-full p-3 rounded bg-emerald-400 text-white hover:bg-emerald-500 duration-200">Login</button>
+		<div class="flex items-center justify-between">
+			<a
+				href="/auth/forgot-password"
+				class="inline-block mb-10 text-xs text-slate-400 hover:text-slate-700 duration-200"
+				>Forgot Password?</a
+			>
+			<a
+				href="/auth/register"
+				class="inline-block mb-10 text-xs text-slate-400 hover:text-slate-700 duration-200"
+				>Sign Up</a
+			>
+		</div>
+
+		<button
+			class="w-full p-3 rounded bg-emerald-400 text-white hover:bg-emerald-500 duration-200"
+			disabled={submitting}
+		>
+			{#if submitting}
+				<Loader />
+				Loading...
+			{:else}
+				Sign In
+			{/if}
+		</button>
 	</form>
 </div>
